d37fd8dab12d1d0458def1da671246f9241ea931
Mirang <72075871+Mirang18@users.noreply.github.com>
2023-01-20 16:19:59 +0530

    Add files via upload
    Contains Directive Implementation
    (PS:Commented one of the approaches for ngFor to avoid conflicts)

--- app.component.ts
+++ app.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
  
 @Component({
-selector:'app-root',
-templateUrl: './app.component.html',
+selector: 'app-root',
+templateUrl:'./app.component.html'
 })
 export class AppComponent {
-  title = 'Mirang';
+   selectedCountry: any;
+
+   
+  countries:any[] = [
+    {code:'ind',country:'India'},
+    {code:'arg',country:'Argentina'},
+    {code:'aus',country:'Australia'},
+    {code:'uk',country:'United Kingdom'},
+    {code:'rsa',country:'Republic Of South Africa'},
+    {code:'urg',country:'Uruguay'}
+  ];
+  choice(code: any){
+    // alert(code)
+    // alert(code.value)
+this.selectedCountry = code.value;
+console.log(code.value);
+
+
+
+  }
+  
+  title:string = 'Mirang';
+  isUserLoggedIn:boolean = false
+
+  // users:string[] = ["Mirang","Atharva","Shashwat"];
+users:any[] = [
+
+{id:101,name:'Mirang',city:'Mumbai'},
+{id:102,name:'Atharva',city:'Pune'},
+{id:103,name:'Shashwat',city:'Delhi'}
+
+];
+
 }
  
